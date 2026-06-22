69f39077d30a445af37a99387dce096faeb00c9d
Bianca <biancadiebe@gmail.com>
2026-06-22 13:55:10 -0300

    Atualizando card de Vendas

--- sitedie/app/page.tsx
+++ sitedie/app/page.tsx
@@ -1,65 +1,49 @@
-import Image from "next/image";
+"use client";
+
+import Header from '../src/components/header/header';
+import Image from 'next/image';
+import {Card} from '../src/components/card/card';
+import styles from './home.module.css'
+import {PiLeafThin} from "react-icons/pi";
+import { IoMdHeartEmpty } from 'react-icons/io';
+import { TbCandle } from 'react-icons/tb';
+import { CardVenda } from '@/src/components/cardVenda/cardVenda';
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<><div className={styles.img}>
+			<Image src="/imagens/velabubble.jpg" alt="fundo" fill className={styles.backgroundImage} />
+
+			<Header />
+			<div className={styles.divTexto}>
+				<h1 className={`${styles['titulo-principal']}`}>
+					Dando aroma para seus momentos
+				</h1>
+				<button className={styles.botao}>Conheça o catálogo</button>
+			</div>
+		</div>
+
+		<h2 className={`${styles['subtitulo']}`}>Sobre o Diê</h2>
+		<p className={`${styles['texto']}`}> Conheça nossos valores </p>
+			<div className={styles.divCards}>
+			<Card icon={<PiLeafThin />} label="100% vegana" />
+			<Card icon={<IoMdHeartEmpty />} label="Feito à mão" />
+			<Card icon={<TbCandle />} label="Personalizadas" />
+		</div>
+
+    <h2 className={`${styles['subtitulo']}`}>Nosso Catalógo</h2>
+		<p className={`${styles['texto']}`}>Descubra nossos aromas</p>
+    <p><a href="#" className={styles['sub-texto']}>Ver todos <FaLongArrowAltRight /></a></p>
+
+		<div className={styles.divVendas}>
+		<CardVenda icon={<MdOutlineShoppingCart />} label="Compre agora" title="Vela de Cera Natural" price="29,90" img="/imagens/velabubble.jpg" />
+		<CardVenda icon={<MdOutlineShoppingCart />} label="Compre agora" title="Vela de Cera Natural" price="29,90" img="/imagens/velabubble.jpg" />
+		<CardVenda icon={<MdOutlineShoppingCart />} label="Compre agora" title="Vela de Cera Natural" price="29,90" img="/imagens/velabubble.jpg" />
+		</div>
+
+	</>
+	);
 }
+ 
